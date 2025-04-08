056c459a34edf7fee6ecfd3e7599dddfaa4c9417
Havyner <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>
2025-04-07 21:25:44 -0300

    commit exercicio1

---
+++ POO Python/Modulo I/Exercicio1.py
@@ -0,0 +1,21 @@
+''' EXERCICIO 1
+Crie uma classe animal com atributos e métodos, posteriormente, crie uma classe que
+herde seus atributos, e possua os seus atributos próprios.
+'''
+
+class Animal():
+    def __init__(self, nome, idade, especie):
+        self.nome = nome
+        self.idade = idade
+        self.especie = especie
+
+    def get_especie(self):
+        print(f"Minha espécie é: {self.especie}")
+
+class Aves(Animal):
+    def __init__(self, nome, idade, especie, categoria):
+        super().__init__(nome, idade, especie)
+        self.categoria = categoria
+
+a_arara = Aves('Arara Azul', '25', 'Arara', 'ave tropical')
+print(vars(a_arara))
